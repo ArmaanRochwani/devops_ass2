fc999e11b826ba660351fc8628906827603be24f
Armaan Rochwani <armaanrochwani4562@gmail.com>
2025-12-17 18:55:17 +0530

    commit

---
+++ Employee4.java
@@ -0,0 +1,8 @@
+public class Employee {
+    int id;
+    String department;
+
+    void show() {
+        System.out.println(id + " - " + department);
+    }
+}
---
+++ Student3.java
@@ -0,0 +1,9 @@
+public class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
